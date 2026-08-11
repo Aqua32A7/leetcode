c4736a466b7575bd610d6a4d3e23785654e57f81
Aqua32A7 <halyalsumanth@gmail.com>
2026-08-11 18:58:27 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (5.07%) - LeetSync

---
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void solve(TreeNode* root,string s,vector<string>& ans){
+
+        if(root==NULL){
+            return;
+        }
+
+        s+=to_string(root->val);
+
+        if(root->left==NULL && root->right==NULL){
+            ans.push_back(s);
+            return;
+
+        }
+    
+        if(root->left) solve(root->left,s,ans);
+        if(root->right) solve(root->right,s,ans);
+    }
+    int sumNumbers(TreeNode* root) {
+        if (root == NULL) return 0; 
+        vector<string> ans;
+        solve(root,"",ans);
+        int sum=0;
+        for(int i=0;i<ans.size();i++){
+            sum+=stoi(ans[i]);
+        }
+        return sum;
+        
+    }
+};
