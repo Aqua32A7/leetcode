94a4c7cc6b21f4115ef1ee3e7788ebf25c8afee9
Aqua32A7 <halyalsumanth@gmail.com>
2026-09-09 18:37:47 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (57.33%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long commas = 0;
+        
+
+        if (n >= 1000) {
+            commas += n - 999;
+        }
+
+        if (n >= 1000000) {
+            commas += n - 999999;
+        }
+
+        if (n >= 1000000000) {
+            commas += n - 999999999;
+        }
+
+        if (n >= 1000000000000LL) {
+            commas += n - 999999999999LL;
+        }
+
+        if (n >= 1000000000000000LL) {
+            commas += n - 999999999999999LL;
+        }
+
+        return commas;
+
+       
+
+
+
+
+
+
+    }
+};
